98c8829c9e6648745caefcf8275937ea5e31d4d7
yaymalaga <yaymalaga@gmail.com>
2020-10-18 01:39:03 +0200

    Initial complete scrapping

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+use team::Team;
 use thirtyfour_sync::prelude::*;
 
 mod driver;
 mod league;
-mod team;
 mod player;
+mod team;
 
 use crate::league::League;
 
 fn main() {
     let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:4444", &caps).unwrap();
-    
-    let leagues_raw_data = League::scrape_leagues_basic(&driver, None);
-    println!("{}", leagues_raw_data.len())
+
+    let mut leagues_data = League::scrape_leagues_basic(&driver, None);
+
+    for league in leagues_data.iter_mut() {
+        League::scrape_league_teams_basic(&driver, league);
+
+        for team in league.teams.iter_mut() {
+            Team::scrape_team_players(&driver, team);
+            println!("{:#?}", leagues_data);
+            panic!();
+        }
+    }
 }
